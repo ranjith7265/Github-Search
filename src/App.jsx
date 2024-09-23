2c82507ffd0f322146c65379f3a25367bbc8593c
Ranjith K <smartranjithvip@gmail.com>
2024-09-24 00:45:21 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Primary</button>
+		</div>
+	);
+}
+
+export default App;
