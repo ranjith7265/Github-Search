d7b7c42e29049f58e8946b81db1b105b4eae19f1
Ranjith K <smartranjithvip@gmail.com>
2024-09-29 18:02:20 +0530

    spinner component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loadingGIF from "../../assets/loading.gif"
+
+function Spinner() {
+	return (
+		<div className="w-full mx-auto mt-20 felx-row items-center">
+			<img src={loadingGIF} alt="loading..."></img>
+		</div>
+	);
+}
+
+export default Spinner;
