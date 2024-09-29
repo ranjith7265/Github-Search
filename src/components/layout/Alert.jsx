794540e0bd52b5b41d7a2819bd73a7b345638e05
Ranjith K <smartranjithvip@gmail.com>
2024-09-29 18:02:49 +0530

    alert componet

---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import AlertContext from '../context/alert/AlertContext'
+import { IoIosAlert } from "react-icons/io";
+
+function Alert() {
+    const {alert} = useContext(AlertContext)
+  return alert !== null && (
+    <p className='flex item-start mb-4'>
+        <IoIosAlert className='mr-2' />
+        {alert.msg}
+    </p>
+  )
+}
+
+export default Alert
