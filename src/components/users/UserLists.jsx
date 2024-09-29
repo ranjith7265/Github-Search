ce3f835bdca1dff37073c3fd12cef2a891446771
Ranjith K <smartranjithvip@gmail.com>
2024-09-29 18:01:58 +0530

    userList and UserItem component

---
+++ src/components/users/UserLists.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import UserItem from "./UserItem";
+import GithubContext from "../context/github/GithubContext.jsx";
+import Spinner from "../layout/Spinner.jsx";
+
+function UserLists() {
+	const { users, loading } = useContext(GithubContext);
+
+	if (!loading) {
+		return (
+			<ul className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+				{users.map((user) => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</ul>
+		);
+	} else {
+		return <Spinner />;
+	}
+}
+
+export default UserLists;
